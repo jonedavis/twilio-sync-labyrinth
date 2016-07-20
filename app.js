0f7e0beafec804e145f630f949869c888c7060eb
David Prothero <david@prothero.com>
2016-07-20 15:07:22 -0700

    Text messaging pairing

--- app.js
+++ app.js
@@ -4,19 +4,18 @@ var path = require('path');
 var Twilio = require('./twilio-temp')
 var isCallerMobile = require('./mobiledetection');
 var getToken = require('./syncSetup');
-var bodyParser = require('body-parser');
 var config = require('./config.js');
 var app = express();
+
+config.sendTextMessages = false;
+config.domain = 'https://twilio-david.ngrok.io';
 
 app.use(express.static('public'));
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     var isMobile = isCallerMobile(req);
 
     if (!isMobile) {
@@ -30,22 +29,23 @@ app.get('/game/:phoneNumber', function(request, response) {
     response.render('mobile', { phoneNumber: request.params.phoneNumber });
 });
 
-app.post('/sendMessage', function(req, res) {
-  var toNumber = req.body.phoneNumber;
+function sendMessage(toNumber) {
+    if (!config.sendTextMessages) {
+        return;
+    }
 
   var client = new Twilio.Twilio(config.accountSid, config.authToken);
   client.messages.create({
-    body: 'Are you ready for your quest? https://twilio-david.ngrok.io/mobile/' + toNumber.replace('+', ''),
+    body: 'Are you ready for your quest? ' + config.domain + '/game/' + toNumber,
     to: toNumber,  // Text this number
     from: config.twilioNumber // From a valid Twilio number
   }, function(err, message) {
     console.log(message.sid);
   });
-
-  res.sendStatus(200);
-});
+}
 
 app.get('/token/:phoneNumber', (request, response) => {
+    sendMessage(request.params.phoneNumber);
     // Serialize the token to a JWT string and include it in a JSON response
     response.send(getToken(request.params.phoneNumber));
 });
