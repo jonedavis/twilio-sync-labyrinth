374b2af7fcf30b922f34d0f0862b9696f1dbb33e
David Prothero <david@prothero.com>
2016-07-28 17:38:22 -0700

    Fixed up home screen

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var express = require('express');
 var http = require('http');
 var path = require('path');
@@ -13,45 +15,53 @@ app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    var isMobile = isCallerMobile(req);
-
-    if (!isMobile) {
-        res.render('index');
-    } else {
-        res.render('mobile-index');
-    }
+  renderHome(req, res);
 });
 
-app.get('/game/:phoneNumber', function(request, response) {
-    response.render('mobile', { phoneNumber: request.params.phoneNumber });
+app.get('/kiosk', function (req, res) {
+  renderHome(req, res, true);
+});
+
+function renderHome(req, res, kiosk) {
+  var isMobile = isCallerMobile(req);
+
+  if (!isMobile) {
+    res.render('index', {kiosk: !!kiosk});
+  } else {
+    res.render('mobile-index');
+  }
+}
+
+app.get('/game/:phoneNumber', function (req, res) {
+  res.render('mobile', { phoneNumber: req.params.phoneNumber });
 });
 
 function sendMessage(toNumber) {
-    if (!config.sendTextMessages) {
-        return;
-    }
+  if (!config.sendTextMessages) {
+    return;
+  }
 
   var client = new Twilio.Twilio(config.accountSid, config.authToken);
   client.messages.create({
     body: 'Are you ready for your quest? ' + config.domain + '/game/' + toNumber,
     to: toNumber,  // Text this number
     from: config.twilioNumber // From a valid Twilio number
-  }, function(err, message) {
+  }, function (err, message) {
     console.log(message.sid);
   });
 }
 
 app.get('/token/:phoneNumber', (request, response) => {
-    sendMessage(request.params.phoneNumber);
-    // Serialize the token to a JWT string and include it in a JSON response
-    response.send(getToken(request.params.phoneNumber));
+  sendMessage(request.params.phoneNumber);
+  // Serialize the token to a JWT string and include it in a JSON response
+  response.send(getToken(request.params.phoneNumber));
 });
 
 app.get('/token-mobile/:phoneNumber', (request, response) => {
-    // Serialize the token to a JWT string and include it in a JSON response
-    response.send(getToken(request.params.phoneNumber + '-mobile'));
+  // Serialize the token to a JWT string and include it in a JSON response
+  response.send(getToken(request.params.phoneNumber + '-mobile'));
 });
 
 http.createServer(app).listen(app.get('port'), function () {
-  console.log("Express server listening on port " + app.get('port'));
+  console.log('Express server listening on port ' + app.get('port'));
 });
